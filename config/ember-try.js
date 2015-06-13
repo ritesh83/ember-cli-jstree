5070eaa0c8dab8615e636d387c9bea033f3f0e6e
Chester Li <yectep@gmail.com>
2015-06-12 22:27:07 -0700

    add ember-try config. require Ember ^1.11.0

---
+++ config/ember-try.js
@@ -0,0 +1,18 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember 1.11',
+      dependencies: {
+        'ember': '1.11.0',
+        'ember-data': '1.0.0-beta.16.1'
+      }
+    },
+    {
+      name: 'Ember 1.12',
+      dependencies: {
+        'ember': '1.12.0-beta.1',
+        'ember-data': '1.0.0-beta.17'
+      }
+    }
+  ]
+};
