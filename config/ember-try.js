6e21ca0b11da6dd7195e3b6fc4861dff77ebf576
Chester Li <chester.li@panasonic.aero>
2015-08-05 13:11:21 -0700

    update travis scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,6 +13,13 @@ module.exports = {
         'ember': '1.12.0-beta.1',
         'ember-data': '1.0.0-beta.17'
       }
+  },
+  {
+    name: 'Ember 1.13.6 with ember-data 1.13.7',
+    dependencies: {
+      'ember': '1.13.6',
+      'ember-data': '1.13.7'
     }
+  }
   ]
 };
